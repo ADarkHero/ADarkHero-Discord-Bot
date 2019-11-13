e65c8801a3275226b5668d5bdb2251ee4d045af3
ADarkHero <maik.riedlsperger@gmail.com>
2019-11-13 11:11:34 +0100

    Fixed #6 !wiki does not work with wrong casing

--- adhdb/bot/Wikipedia.cs
+++ adhdb/bot/Wikipedia.cs
@@ -47,37 +47,27 @@ namespace adhdb.bot
 				if (stringPairs.Length > 1)
 				{
 					String search = Msg.Content.Remove(0, Msg.Content.IndexOf(' ') + 1).Replace(" ", "%20");
-					String url = "https://de.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&explaintext=1&titles=" + search;
-					String returnString = "";
-					using (WebClient wc = new WebClient())
-					{
-						wc.Encoding = Encoding.UTF8;
-						var xml = wc.DownloadString(url);
+					String corrSearch = SearchOnWikipedia(search);
 
-						//Create xml document and select node
-						XDocument document = XDocument.Parse(xml);
-						var extract = document.Descendants("extract");
-
-						foreach (var foo in extract)
-						{
-							//Put link in front of the string
-							returnString = "https://de.wikipedia.org/wiki/" + search + "\r\n" + foo.Value;
-
-							//We don't want to spam everything... We only want the first paragraph.
-							if (returnString.Contains("=="))
-							{
-								returnString = returnString.Substring(0, returnString.IndexOf("=="));
-							}
-						}
-					}
+					String returnString = GetWikipediaArticle(corrSearch);
 
 					//Check if the search term actually exists.
 					if (String.IsNullOrEmpty(returnString))
 					{
-						//The term was not found. Wikipedia considers casing of the searches.
-						return "Der Begriff wurde leider nicht gefunden. Wikipedia achtet meistens auf die Groß-/Kleinschreibung des Suchbegriffes!";
+						//The term was not found.
+						return "Der Begriff wurde leider nicht gefunden.";
 					}
+					else
+					{
+						//Put link in front of the string
+						returnString = "https://de.wikipedia.org/wiki/" + corrSearch + "\r\n" + returnString;
 
+						//We don't want to spam everything... We only want the first paragraph.
+						if (returnString.Contains("=="))
+						{
+							returnString = returnString.Substring(0, returnString.IndexOf("=="));
+						}
+					}
 
 					return returnString;
 				}
@@ -90,9 +80,88 @@ namespace adhdb.bot
 			catch (Exception ex)
 			{
 				Logger logger = new Logger(ex.ToString());
-				return ex.ToString();
+				//Unknown error.
+				return "Unbekannter Fehler.\r\n\r\n" + ex.ToString();
 			}
 
 		}
+
+		/// <summary>
+		/// Searches a term on wikipedia and returns the first result.
+		/// </summary>
+		/// <param name="search">The term you want to search Wikipedia for.</param>
+		/// <returns>The name of the first Wikipedia result.</returns>
+		private static string SearchOnWikipedia(String search)
+		{
+			try
+			{
+				String sUrl = "https://de.wikipedia.org/w/api.php?action=query&format=xml&list=search&srsearch=" + search;
+
+				String corrSearch = "";
+				using (WebClient wc = new WebClient())
+				{
+					wc.Encoding = Encoding.UTF8;
+					var xml = wc.DownloadString(sUrl);
+
+					//Create xml document and select node
+					XDocument document = XDocument.Parse(xml);
+					//Get all search entries (p)
+					var extract = document.Descendants("p");
+
+					//Return the first entry.
+					foreach (var foo in extract)
+					{
+						corrSearch = foo.Attribute("title").Value;
+						if (!String.IsNullOrEmpty(corrSearch)) { break; }
+					}
+				}
+
+				return corrSearch;
+			}
+			catch (Exception ex)
+			{
+				Logger logger = new Logger(ex.ToString());
+				//Unknown error.
+				return "Unbekannter Fehler.\r\n\r\n" + ex.ToString();
+			}
+		}
+
+		/// <summary>
+		/// Displays the text of a Wikipedia article
+		/// </summary>
+		/// <param name="search">The article you want to dispay. Note: The article must exist!</param>
+		/// <returns>The text of the article.</returns>
+		private static string GetWikipediaArticle(String search)
+		{
+			try
+			{
+				String url = "https://de.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&explaintext=1&titles=" + search;
+
+				using (WebClient wc = new WebClient())
+				{
+					wc.Encoding = Encoding.UTF8;
+					var xml = wc.DownloadString(url);
+
+					//Create xml document and select node
+					XDocument document = XDocument.Parse(xml);
+					var extract = document.Descendants("extract");
+
+					foreach (var foo in extract)
+					{
+						return foo.Value;
+					}
+				}
+
+				return "";
+			}
+			catch (Exception ex)
+			{
+				Logger logger = new Logger(ex.ToString());
+				//Unknown error.
+				return "Unbekannter Fehler.\r\n\r\n" + ex.ToString();
+			}
+		}
+
+
 	}
 }
